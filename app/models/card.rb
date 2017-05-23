30353412c14330bf2a1ad36173108b20a2db32bc
Marina <belova.ma.ev@gmail.com>
2017-05-23 15:02:50 +0300

    Add card checking

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,10 +2,17 @@ class Card < ApplicationRecord
   validates :original_text, :translated_text, :review_date, presence: true
   validate :text_uniqueness
 
+  scope :needed_to_review,  -> { where('review_date <= ?', Date.today) }
+
+
+  def correct_translate?(word)
+    word.strip.downcase == original_text.strip.downcase
+  end
+
   private
 
   def text_uniqueness
-    if self.original_text.strip.downcase == self.translated_text.strip.downcase
+    if original_text.strip.downcase == translated_text.strip.downcase
       errors.add(:translated_text, 'Текст должен быть разным')
       errors.add(:original_text, 'Текст должен быть разным')
     end
