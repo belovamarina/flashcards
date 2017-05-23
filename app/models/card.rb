30fc6b0925c76aa9bba042e1af9973ce07303617
Marina <belova.ma.ev@gmail.com>
2017-05-23 15:23:59 +0300

    Fix hound comments

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,19 +2,17 @@ class Card < ApplicationRecord
   validates :original_text, :translated_text, :review_date, presence: true
   validate :text_uniqueness
 
-  scope :needed_to_review,  -> { where('review_date <= ?', Date.today) }
-
+  scope :needed_to_review, -> { where('review_date <= ?', Date.today) }
 
   def correct_translate?(word)
-    word.strip.downcase == original_text.strip.downcase
+    word.strip.casecmp(original_text.strip).zero?
   end
 
   private
 
   def text_uniqueness
-    if original_text.strip.downcase == translated_text.strip.downcase
-      errors.add(:translated_text, 'Текст должен быть разным')
-      errors.add(:original_text, 'Текст должен быть разным')
-    end
+    return unless original_text.strip.casecmp(translated_text.strip).zero?
+    errors.add(:translated_text, 'Текст должен быть разным')
+    errors.add(:original_text, 'Текст должен быть разным')
   end
 end
