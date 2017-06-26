128b05d7bba2b68d1bb139a9bb54863f558a1f17
Marina <belova.ma.ev@gmail.com>
2017-06-26 20:38:34 +0300

    Add specs for NotificationsMailer

---
+++ spec/mailers/notifications_mailer_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe NotificationsMailer, type: :mailer do
+  describe 'notify' do
+    let(:user) { create(:user_with_decks) }
+    let(:mail) { NotificationsMailer.pending_cards(user) }
+
+    it 'renders the headers' do
+      expect(mail.subject).to match(user.email)
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq([ENV['EMAIL']])
+    end
+
+    it 'renders the body' do
+      card = user.cards.where('review_date <= ?', Time.now).first
+      expect(mail.body.encoded).to match(card.translated_text)
+    end
+  end
+end
