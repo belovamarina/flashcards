fea95bc394a74a26c60f815215e326b7306f6ff5
Marina <belova.ma.ev@gmail.com>
2017-07-04 12:37:21 +0300

    Fix migrations

--- db/migrate/20170702125357_add_e_factor_and_interval_to_cards.rb
+++ db/migrate/20170702125357_add_e_factor_and_interval_to_cards.rb
@@ -1,4 +1,6 @@
 class AddEFactorAndIntervalToCards < ActiveRecord::Migration[5.0]
   def change
+    add_column :cards, :e_factor, :decimal, precision: 6, scale: 2, null: false, default: 2.5
+    add_column :cards, :interval, :integer, null: false, default: 0
   end
 end
